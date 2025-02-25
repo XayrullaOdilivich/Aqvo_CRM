c1f3d7fe8fb459d3d5594a9ebaa48c80a256a0cb
xayrullo <xayrullo936@gmail.com>
2025-02-25 18:59:43 +0500

    update store.vue

--- src/components/Stores.vue
+++ src/components/Stores.vue
@@ -62,8 +62,9 @@ onMounted(async () => {
 const updateShops = async (id) => {
     try {
         const updatedData = {
-            category: selectedShops.value.category,
-            unit: selectedShops.value.unit
+            address: selectedShops.value.address,
+            name: selectedShops.value.name,
+            phone: selectedShops.value.phone
         };
         await updateStore.fetchPut(`/stores/${selectedShops.value.id}`, updatedData, {
             headers: {
@@ -106,6 +107,7 @@ const deleteShops = async (id) => {
                     <h1>Magazini tahrirlash</h1>
                     <label for="exampleFormControlInput1" class="form-label">Magazin Nomi</label>
                     <input
+                        v-model="selectedShops.name"
                         type="text"
                         class="form-control"
                         aria-label="Sizing example input"
@@ -115,6 +117,7 @@ const deleteShops = async (id) => {
                     >
                     <label for="exampleFormControlInput1" class="form-label">Manzili</label>
                     <input
+                        v-model="selectedShops.address"
                         type="text"
                         class="form-control"
                         aria-label="Sizing example input"
@@ -124,6 +127,7 @@ const deleteShops = async (id) => {
                     >
                     <label for="exampleFormControlInput1" class="form-label">Telefon raqami</label>
                     <input
+                        v-model="selectedShops.phone"
                         type="tel"
                         class="form-control"
                         aria-label="Sizing example input"
@@ -131,7 +135,7 @@ const deleteShops = async (id) => {
                         placeholder="+998 99 999 99 99"
                         required
                     >
-                    <button @click="closedModal" class="btn btn-primary">Yuborish</button>
+                    <button @click="updateShops" class="btn btn-primary">Yuborish</button>
                 </div>
             </div>
         </div>
@@ -191,33 +195,13 @@ const deleteShops = async (id) => {
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Makro</td>
-                            <td>Seul ko'chasi, Besh-Yog`och mahallasi, Chilonzor Tumani, Toshkent, 100000, Oʻzbekiston</td>
-                            <td>+998 99 999 99 99</td>
-                            <td>
-                                <button class="update" @click="updateModal">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
-                                    </svg>
-                                </button>
-                                <button class="delete">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0"/>
-                                    </svg>
-                                </button>
-                            </td>
-                        </tr>
-                        </tbody>
-                        <tbody>
                         <tr v-for="(shops, idx) in shops" :key="shops.id">
                             <td>{{ idx + 1 }}</td>
                             <td>{{ shops.name }}</td>
                             <td> {{ shops.address }}</td>
                             <td>{{ shops.phone }}</td>
                             <td>
-                                <button class="update" @click="updateModal">
+                                <button class="update" @click="updateModal(shops)">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
                                         <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.5.5 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11z"/>
                                     </svg>
